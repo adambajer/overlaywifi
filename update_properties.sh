836e8c52b9cc888794d2631e3c1aca4eaeeef253
adambajer <adam.bajer@gmail.com>
2025-05-12 23:06:11 +0200

    Create update_properties.sh

---
+++ update_properties.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# file: update_properties.sh
+touch local.properties
+echo "BASE_URL=\"$1\""        >> local.properties
+echo "MAP_KEY=\"$2\""         >> local.properties
+echo "RETRY_ATTEMPTS=$3"      >> local.properties
+echo "THRESHOLD_VALUE=$4"     >> local.properties
